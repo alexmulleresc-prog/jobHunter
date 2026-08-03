10a04c0fac16a4e7b09a13998d98b15ab4b97800
alex <alexmulleresc@gmail.com>
2026-08-03 12:30:44 -0300

    Coneccion del scaper con el repository

--- backend/services/job_service.py
+++ backend/services/job_service.py
@@ -1,11 +1,16 @@
 from backend.scrapers.remoteok import RemoteOKScraper
+from backend.repositories.job_repository import JobRepository
 
 class JobService:
     def __init__(self):
         self.scrapers = [RemoteOKScraper()]
+        self.repository = JobRepository()
 
     def obtener_empleos(self):
         empleos =[]
         for scraper in self.scrapers:
-            empleos.extend(scraper.buscar_empleos())
+            jobs = scraper.buscar_empleos()
+            for job in jobs:
+                self.repository.create(job)
+            empleos.extend(jobs)
         return empleos
